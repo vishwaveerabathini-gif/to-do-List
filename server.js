e5bb6e8a520fdc55eaa15660985025d2a868e91f
Vishwa Teja <vishwaveerabathini@gmail.com>
2026-08-19 00:12:05 +0530

    Serve index page after login

--- server.js
+++ server.js
@@ -24,6 +24,10 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"));
 });
 
+app.get("/index.html", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
 app.get("/login.html", (req, res) => {
     res.sendFile(path.join(__dirname, "login.html"));
 });
